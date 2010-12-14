84d2eb78b309d09390e64df8758b74d294df8edb
bluerainbow <the.world.lords@gmail.com>
2010-12-14 13:33:12 +0100

    Vytvoreny MessageFlowManagement

--- XRouter.Experiments_MC/Simulations/Tests/TestSimulation2.cs
+++ XRouter.Experiments_MC/Simulations/Tests/TestSimulation2.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using XRouter.Simulator;
 using XRouter.Experiments.Permanent.MC.Components.Common;
-using XRouter.Experiments.Permanent.MC.Components.Version1;
+using XRouter.Experiments.Permanent.MC.Components.Samples;
 
 namespace XRouter.Experiments_MC.Simulations.Tests
 {
@@ -13,26 +13,19 @@ namespace XRouter.Experiments_MC.Simulations.Tests
         public TestSimulation2()
             : base("MC_TestSimulation2")
         {
-            var sender1 = new ManualMessageSender("Sender1");
-            var sender2 = new ManualMessageSender("Sender2");
-            var display1 = new MessageDisplay("Display1");
-            var display2 = new MessageDisplay("Display2");
-            var workflowProcessor = new BroadcastingWorkflowProcessor("Broadcaster", 2, 2);
+            var sender1 = new ManualMessageSender_v1("Sender1");
+            var sender2 = new ManualMessageSender_v1("Sender2");
+            var display1 = new MessageDisplay_v1("Display1");
+            var display2 = new MessageDisplay_v1("Display2");
+            var workflowProcessor = new BroadcastingMessageFlowProcessor_v1("Broadcaster", 2, 2);
 
-            var sender1_wfp = new Connector(sender1, workflowProcessor.ConnectableInputPoints[0]);
-            var sender2_wfp = new Connector(sender2, workflowProcessor.ConnectableInputPoints[1]);
-            var wfp_display1 = new Connector(workflowProcessor.ConnectableOutputPoints[0], display1);
-            var wfp_display2 = new Connector(workflowProcessor.ConnectableOutputPoints[1], display2);
+            var sender1_wfp = new Connector_v1(sender1, workflowProcessor.ConnectableInputPoints[0]);
+            var sender2_wfp = new Connector_v1(sender2, workflowProcessor.ConnectableInputPoints[1]);
+            var wfp_display1 = new Connector_v1(workflowProcessor.ConnectableOutputPoints[0], display1);
+            var wfp_display2 = new Connector_v1(workflowProcessor.ConnectableOutputPoints[1], display2);
 
-            //Components.Add(sender1_wfp);
-            //Components.Add(sender2_wfp);
-            //Components.Add(wfp_display1);
-            //Components.Add(wfp_display2);
             Components.Add(sender1);
             Components.Add(sender2);
-            Components.Add(display1);
-            Components.Add(display2);
-            Components.Add(workflowProcessor);
         }
     }
 }
